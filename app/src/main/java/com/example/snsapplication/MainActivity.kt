8dd1444fd860e8e5f4af9acf028651b4a7d0849d
Jsueeee <mmmm07@naver.com>
2020-08-16 01:47:53 +0900

    Add Navigation (MainActivity)

--- app/src/main/java/com/example/snsapplication/MainActivity.kt
+++ app/src/main/java/com/example/snsapplication/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.snsapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.snsapplication.navigation.AlarmFragment
+import com.example.snsapplication.navigation.DetailViewFragment
+import com.example.snsapplication.navigation.GridFragment
+import com.example.snsapplication.navigation.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        //이벤트 넣어주기
+        when (p0.itemId) {
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, alarmFragment).commit()
+                return true
+            }
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, userFragment).commit()
+                return true
+            }
+        }
+        return false //위 값을 만족하지 못할 때
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)//리스너연결
     }
 }
