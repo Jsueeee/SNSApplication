2daa728c15505b5ce148ced8d269f28340630eec
Jsueeee <mmmm07@naver.com>
2020-08-18 02:14:08 +0900

    Add Storage Upload Service in PhotoActivity

---
+++ app/src/main/java/com/example/snsapplication/navigation/AddPhotoActivity.kt
@@ -0,0 +1,64 @@
+package com.example.snsapplication.navigation
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.snsapplication.R
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_add_photo.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddPhotoActivity : AppCompatActivity() {
+    var PICK_IMAGE_FROM_ALBUM = 0
+    var storage : FirebaseStorage? = null
+    var photoUri : Uri? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_photo)
+
+        //Initate Storage
+        storage = FirebaseStorage.getInstance()
+
+        //Open the album
+        var photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.type = "image/*"
+        startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM)
+
+        //add image upload event
+        addPhoto_btn_upload.setOnClickListener {
+            contentUpload()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == PICK_IMAGE_FROM_ALBUM){
+            if(requestCode == Activity.RESULT_OK){
+                //사진을 선택했을 때 이미지의 경로가 여기로 넘어옴
+                photoUri = data?.data //photoUri에 경로 담아주기
+                addPhoto.setImageURI(photoUri) //이미지뷰에 선택한 이미지 보여주기
+
+            }else{
+                //취소 버튼 눌렀을 때
+                finish()
+            }
+        }
+    }
+    fun contentUpload(){
+        //make filename
+
+        var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        var imageFileName = "IMAGE_" + timestamp + "_.png"
+
+        var storageRef = storage?.reference?.child(imageFileName)
+
+        //FileUpload
+        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+            Toast.makeText(this, getString(R.string.upload_success), Toast.LENGTH_LONG).show()
+        }
+    }
+}
