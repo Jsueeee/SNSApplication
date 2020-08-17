11d8b135dad244586436f523bf521ae50e80074f
Jsueeee <mmmm07@naver.com>
2020-08-18 02:19:59 +0900

    Add Storage Permission Request & Check in MainActivity

--- app/src/main/java/com/example/snsapplication/MainActivity.kt
+++ app/src/main/java/com/example/snsapplication/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.snsapplication
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import com.example.snsapplication.navigation.AlarmFragment
-import com.example.snsapplication.navigation.DetailViewFragment
-import com.example.snsapplication.navigation.GridFragment
-import com.example.snsapplication.navigation.UserFragment
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.snsapplication.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -29,7 +31,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo -> {
-
+                //외부 스토리지에서 가져올 수 있는 권한이 있는지 체크
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                }
                 return true
             }
             R.id.action_favorite_alarm -> {
@@ -51,5 +56,8 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)//리스너연결
+        //사진 경로를 가져올 수 있는 권한 요청
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)//manifest->android
     }
+
 }
