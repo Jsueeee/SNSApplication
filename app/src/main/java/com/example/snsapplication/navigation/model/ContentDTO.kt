1d4fafa761155393783d2163521a05091306b849
Jsueeee <mmmm07@naver.com>
2020-08-19 00:51:06 +0900

    Add ContentDTO.kt

---
+++ app/src/main/java/com/example/snsapplication/navigation/model/ContentDTO.kt
@@ -0,0 +1,16 @@
+package com.example.snsapplication.navigation.model
+
+data class ContentDTO(var explain : String? = null,
+                      var imageUri : String? = null,
+                      var uid : String? = null,
+                      var userId : String? = null,
+                      var timestamp : Long? = null,
+                      var favoriteCount : Int = 0,
+                      //중복 좋아요 방지할 수 있도록 유저 정보 관리
+                      var favorites : Map<String, Boolean> = HashMap()){
+    //댓글 관리
+    data class Comment(var uid : String? = null,
+                       var userId : String? = null,
+                       var comment : String? = null,
+                       var timestamp : Long? = null)
+}
