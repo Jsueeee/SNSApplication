e08f1831350aa762e22ddd8df71de88270e242fa
Jsueeee <66951780+Jsueeee@users.noreply.github.com>
2020-09-23 01:01:27 +0900

    Add Print Input Message

--- app/src/main/java/com/example/snsapplication/navigation/commentActivity.kt
+++ app/src/main/java/com/example/snsapplication/navigation/commentActivity.kt
@@ -1,12 +1,23 @@
 package com.example.snsapplication.navigation
 
+import android.graphics.RectF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.snsapplication.R
 import com.example.snsapplication.navigation.model.ContentDTO
+import com.google.api.Distribution
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_comment.*
+import kotlinx.android.synthetic.main.item_comment.view.* //확인필요
+import java.net.ConnectException
 
 class commentActivity : AppCompatActivity() {
     var contentUid : String? = null
@@ -14,6 +25,10 @@ class commentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comment)
         contentUid = intent.getStringExtra("contentUid") //intent에서 넘어온 스트링값 세팅
+
+        //comment리사이클러뷰랑 어댑터 연결
+        comment_recyclerview.adapter = CommentRecyclerViewAdapter()
+        comment_recyclerview.layoutManager = LinearLayoutManager(this)
 
         comment_btn_send?.setOnClickListener {
             var comment = ContentDTO.Comment()
@@ -27,5 +42,57 @@ class commentActivity : AppCompatActivity() {
 
             comment_edit_message.setText("")
         }
+    }
+    inner class CommentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+        var comments : ArrayList<ContentDTO.Comment> = arrayListOf()//초기화
+        init{
+            //파이어베이스 데이터를 읽어오는 코드
+            FirebaseFirestore.getInstance()
+                .collection("images")
+                .document(contentUid!!)
+                .collection("comments")
+                .orderBy("timestamp")//시간순
+                .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    comments.clear()//값 중복 위해서
+                    if(querySnapshot == null) return@addSnapshotListener
+
+                    for(snapshot in querySnapshot.documents!!){
+                        comments.add(snapshot.toObject(ContentDTO.Comment::class.java)!!)
+                    }
+                    notifyDataSetChanged()//리사이클러뷰 새로고침
+                }
+
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_comment, parent, false)
+            return CustomViewHolder(view)
+        }
+
+        private inner class CustomViewHolder(view : View) : RecyclerView.ViewHolder(view)
+
+        override fun getItemCount(): Int {
+            return comments.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            //서버에서 넘어온 아이디랑 메시지 매핑
+            var view = holder.itemView
+            view.commentviewitem_textview_comment.text = comments[position].comment
+            view.commentviewitem_textview_profile.text = comments[position].userId
+
+            FirebaseFirestore.getInstance()
+                .collection("profileImages")
+                .document(comments[position].uid!!)
+                .get()
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        var url = task.result!!["image"]
+                        Glide.with(holder.itemView.context).load(url).apply(RequestOptions().circleCrop()).into(view.commentviewitem_imageview_profile)
+                    }
+                }
+
+        }
+
     }
 }
